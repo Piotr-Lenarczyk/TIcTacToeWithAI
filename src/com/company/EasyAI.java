3d046ecb0cfe74ca75c3bcc0d2e2a6450353e267
Piotr-Lenarczyk <85508623+Piotr-Lenarczyk@users.noreply.github.com>
2021-06-10 19:20:03 +0200

    Update EasyAI.java

--- src/com/company/EasyAI.java
+++ src/com/company/EasyAI.java
@@ -12,6 +12,8 @@ public class EasyAI extends AI {
     @Override
     public void makeAMove(char player) {
         LinkedList<Integer> linkedList = new LinkedList<>();
+        
+        //Add each empty cell to the list
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (getGridElement(this.grid, i * 3 + j) != 'X' && getGridElement(this.grid, i * 3 + j) != 'O') {
@@ -19,6 +21,8 @@ public class EasyAI extends AI {
                 }
             }
         }
+        
+        //Choose random empty cell
         Random random = new Random();
         int index = random.nextInt(linkedList.size());
         setGridElement(this.grid, linkedList.get(index), player);
